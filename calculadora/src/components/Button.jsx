1c0b675c0e7769c22cb42837e748637b6807040e
WilianPer <wsresilienttasteshopw@gmail.com>
2022-06-17 22:29:29 -0400

    Criando as ações e os estilos CSS do BUttons!

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 import './Button.css'
 
+// export default props => {
+    // let classes = 'button '
+    // classes += props.operation ? 'operation' : ''
+    // classes += props.double ? 'double' : ''
+    // classes += props.triple ? 'triple' : ''
+    // return (
+        // <button 
+            // onClick={e => props.click && props.click(e.target.label)}
+            // className={classes}>
+            // {props.label}
+        // </button>
+//     )
+// }
+//         {props.label}
+//     </button>
+
 export default props => 
-    <button className='button'>{props.label}</button>
+    <button 
+    onClick={e => props.click && props.click(e.target.label)}
+    className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+    `}>
+        {props.label}
+    </button>
